d09c5b8885fc69cc23a76e12750309538717071e
Josh Brody <josh@josh.mn>
2020-12-05 17:56:04 -0600

    fix weird heroku quirk that puts the dir into app"

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -4,6 +4,10 @@ module CampaignsHelper
   end
 
   def dripper_source(campaign)
-    File.read(Rails.root.join("app", "drippers", dripper_filename(campaign)))
+    if Rails.root.to_s.include?("app") # we're on heroku
+      File.read(Rails.root.join("drippers", dripper_filename(campaign)))
+    else
+      File.read(Rails.root.join("drippers", dripper_filename(campaign)))
+    end
   end
 end
