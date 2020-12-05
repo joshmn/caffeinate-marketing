293e4ebb4684a28b9b23a0e7ebaa10fe8edd4466
Josh Brody <josh@josh.mn>
2020-12-05 17:59:16 -0600

    fix naming for dripper filename

--- app/helpers/campaigns_helper.rb
+++ app/helpers/campaigns_helper.rb
@@ -1,13 +1,13 @@
 module CampaignsHelper
   def dripper_filename(campaign)
-    "#{campaign.name.parameterize.underscore.delete_prefix("caffeinate_")}_dripper.rb"
+    "#{campaign.to_dripper.name.delete_suffix("Dripper").parameterize.underscore}_dripper.rb"
   end
 
   def dripper_source(campaign)
     if Rails.root.to_s.include?("app") # we're on heroku
       File.read(Rails.root.join("drippers", dripper_filename(campaign)))
     else
-      File.read(Rails.root.join("drippers", dripper_filename(campaign)))
+      File.read(Rails.root.join("app", "drippers", dripper_filename(campaign)))
     end
   end
 end
