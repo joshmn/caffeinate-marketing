f70599f983b30b80d0f19740c63681bb9dd31c84
Josh Brody <git@josh.mn>
2021-09-14 12:23:09 -0500

    fix layout

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,5 @@
 class ApplicationController < ActionController::Base
-  layout Proc.new { |controller| controller.devise_controller? ? "devise" : "application" }
+  layout Proc.new { |controller| controller.devise_controller? ? "devise" : "base" }
 
   def set_page_title(val)
     @page_title = val
